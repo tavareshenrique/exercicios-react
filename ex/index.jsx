9b6f94f33bbd34f0d42b6249992a8faf732a9f1a
Henrique Tavares <ihenrits@gmail.com>
2018-09-25 17:32:29 -0300

    Configurando o Redux

--- ex/index.jsx
+++ ex/index.jsx
@@ -1,5 +1,15 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
+import { combineReducers, createStore } from 'redux'
+import { Provider } from 'react-redux'
 import Field from './field'
 
-ReactDOM.render(<Field initialValue='Henrique' />, document.getElementById('app'))
+const reducers = combineReducers({
+    field: () => ({value: 'Opa'})
+})
+
+ReactDOM.render(
+    <Provider store={createStore(reducers)}>
+        <Field initialValue='Henrique' />
+    </Provider>
+    ,document.getElementById('app'))
